1af67ffdd82d74956c4a285d4199de7508f60e12
蔡咏钦 <852426401@.qq.com>
2026-09-15 15:33:16 +0800

    update: 修复登出弹窗

--- project/wikiAdmin/src/main.js
+++ project/wikiAdmin/src/main.js
@@ -5,6 +5,10 @@ import App from './App.vue'
 import router from './router'
 
 // 公共样式统一入口（规范第 3.1 章）
+// ElMessage / ElMessageBox 等编程式组件不走模板解析器，需显式引入样式
+import 'element-plus/es/components/base/style/css'
+import 'element-plus/es/components/message/style/css'
+import 'element-plus/es/components/message-box/style/css'
 import '@/styles/index.scss'
 
 const app = createApp(App)

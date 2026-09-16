dbbc8d5f61732610f9d164a4fc0f74b80430bbb0
蔡咏钦 <852426401@.qq.com>
2026-09-16 15:42:16 +0800

    update: wiki web 端口替换到 8086

--- project/wikiWeb/vite.config.js
+++ project/wikiWeb/vite.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
     open: false,
     proxy: {
       '/api': {
-        target: 'http://localhost:8085',
+        target: 'http://localhost:8086',
         changeOrigin: true
       }
     }
